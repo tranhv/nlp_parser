95b425fbecafbf6f6ff23a7363242cf06c48cf47
hvt <hvt@hvts-macpro.local>
2016-03-19 23:43:45 +0700

    Fix bugs

--- code/get_data.rb
+++ code/get_data.rb
@@ -2,60 +2,53 @@ class Get_data
   INPUT_PATH = "./input"
   OUTPUT_PATH = "./output"
 
-	def get_data(in_path,out_path_source,out_path_target)
-		begin
-			first_line = ''
-			contents = ''
+  def get_data(in_path,source_file,target_file)
+    begin
+     first_line = ''
+     contents = ''
 
-			flag = false
-			source_file = File.open(out_path_source,"a+")
-			target_file = File.open(out_path_target,"a+")
+     flag = false
+      file = File.open(in_path, "r").each do |line|
+        if (flag == true)
+          flag = false
+          target_file.write(line)
+          next
+        end
+        next if line.match(/^</)      # if the first character of the line is < --> skip this line
+        #next if !/\A\d+\z/.match(line[0,1]) 
+        next if line.strip.match(/\A\d+\z/)     # if the line is numeric --> skip this line
+        source_file.write(line)
+        flag = true             # if we have written a line --> mark the flag and we will skip the next line
+      end      
+    rescue Exception => e
+      puts "Error happened in #{e.message}"
+    end
 
-			#source_file.write("gigi start ------- #{in_path}\n")
-			#target_file.write("gigi start ------- #{in_path}\n")
-			file = File.open(in_path, "r").each do |line|
+  end
+  def main
+    files = Dir.glob(INPUT_PATH + "/full.txt").entries
 
-				if (flag == true)
-					flag = false
-					target_file.write(line)
-					next
-				end
-				next if line.match(/^</)			# if the first character of the line is < --> skip this line
-				#next if !/\A\d+\z/.match(line[0,1]) 
-				next if line.strip.match(/\A\d+\z/)			# if the line is numeric --> skip this line
-				source_file.write(line)
-				flag = true							# if we have written a line --> mark the flag and we will skip the next line
-			end
+    source_file = File.open("./output/source2.txt","w")
+    source_file.write("")
 
-			#source_file.write("gigi end ------- #{in_path}\n")
-			#target_file.write("gigi end ------- #{in_path}\n")
-		
-		rescue Exception => e
-			puts "Error happened in #{e.message}"
-		end
+    target_file = File.open("./output/target2.txt","w")
+    target_file.write("")
 
-	end
-	 def main
-    files = Dir.new(INPUT_PATH).entries
-	
-	source_file = File.open("./output/source.txt","w")
-	source_file.write("")
+    source_file = File.open(source_file,"a+")
+    target_file = File.open(target_file,"a+")
 
-	target_file = File.open("./output/target.txt","w")
-	target_file.write("")
-	
+
     files.each do |file|
       if file != "." and file != ".." and file != ".DS_Store"
-        in_path = "#{INPUT_PATH}/#{file}"
-        out_source_path = "#{OUTPUT_PATH}/source.txt"
-        out_target_path = "#{OUTPUT_PATH}/target.txt"
-
-		puts "in_put: #{in_path}"
-		puts "out_put: #{out_source_path} - #{out_target_path}"
-		get_data(in_path,out_source_path,out_target_path)
-
+        puts "in_put: #{file}"
+        puts "out_put: #{source_file.inspect} - #{target_file.inspect}"
+        get_data(file,source_file,target_file)
       end
     end
+
+    target_file.close
+    source_file.close
+
   end
 end
 
---
+++ code/join.rb
@@ -0,0 +1,43 @@
+class Join
+  INPUT_PATH = "./input"
+  OUTPUT_PATH = "./output"
+  
+  def join_files(in_path,out_path, join_file)
+
+  	begin
+      first_line = ''
+      contents = ''
+      file = File.open(in_path, "r").each do |line|
+       join_file.write(line)
+     end
+
+   rescue Exception => e
+    puts "Error happened in #{e.message}"
+  end
+  
+end
+
+def main
+  files = Dir.glob(INPUT_PATH + "/SWA/*.crp").entries
+
+  join_file = File.open("./output/full.txt","w")
+  join_file.write("")
+  out_path = "#{OUTPUT_PATH}/full.txt"
+  join_file = File.open(out_path,"a+")
+
+  files.each do |file|
+    if file != "." and file != ".." and file != ".DS_Store"
+      in_path = "#{file}"
+      
+      puts "in_put: #{in_path}"
+      puts "out_put: #{out_path}"
+      join_files(in_path,out_path,join_file)
+    end
+  end
+  join_file.close
+
+end
+
+end
+obj = Join.new
+obj.main
