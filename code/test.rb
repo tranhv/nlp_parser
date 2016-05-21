a1bcfafafdd73e2fec3d085d67a34037657a990c
hvt <hvt@hvts-macpro.local>
2016-05-21 18:06:38 +0700

    update test

--- code/test.rb
+++ code/test.rb
@@ -1,9 +1,19 @@
-require './read_data.rb'
+#require './read_data.rb'
+require "json"
 
 class Test
   def main
-    read = ReadData.new
-    read.main
+    # read = ReadData.new
+    # read.main
+    read_json
+  end
+
+  def read_json
+    file = File.read("./data/sample_json.json")
+    data = JSON.parse(file)
+    puts "data"
+    puts "#{data.inspect}"
+    
   end
 end
 
